d444196d0762f958a4bbe1f6931c1e3e3fef23d2
NasirAli1419 <1419nasir@gmail.com>
2025-12-11 20:45:16 +0530

    In Progress

---
+++ dbInit.js
@@ -0,0 +1,32 @@
+const fs = require("fs");
+const mysql = require("mysql2/promise");
+
+async function initDB() {
+  try {
+    // Read SQL file
+    const schema = fs.readFileSync(".src/db/schema.sql", "utf8");
+
+    // Connect without selecting DB (because DB may not exist yet)
+    const connection = await mysql.createConnection({
+      host: "localhost",
+      user: "root",
+      password: "Nasir@1419",
+      multipleStatements: true,
+    });
+
+    await connection.query("CREATE DATABASE IF NOT EXISTS mastero");
+
+    // 3️⃣ Switch to the database
+    await connection.query("USE mastero");
+
+    // Execute schema
+    await connection.query(schema);
+
+    console.log("✅ Database and tables initialized successfully!");
+    await connection.end();
+  } catch (err) {
+    console.error("❌ Error initializing DB:", err);
+  }
+}
+
+initDB();
